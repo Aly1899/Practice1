a586c7a8b95cfd805195fba9c72b966f3f966201
aly1899 <alpar1899@gmail.com>
2019-01-25 23:31:55 +0100

    2d matrix list

---
+++ 2d_matrix.py
@@ -0,0 +1,12 @@
+x = input("Please enter the first number: ")
+y = input("Please enter the second number: ")
+
+i_array = []
+j_array = []
+
+for i in range(int(x)):
+    for j in range(int(y)):
+        j_array.append(i*j)
+    i_array.append(j_array)
+    j_array = []
+print(i_array)
